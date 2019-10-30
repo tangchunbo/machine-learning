4874a0a6e83635bfd3863198a3b799d98e93c4e8
tangchunbo <532066967@qq.com>
2019-10-30 14:55:48 +0800

    add svd

--- ml/svd/svd-1.py
+++ ml/svd/svd-1.py
@@ -8,11 +8,10 @@ file_path = "./img/img.jpg"
 image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
 m, n = image.shape
 
+# 原本L应该时一个对称矩阵，此时则将对称矩阵的对角线元素以一维数组形式输出
 P, L, Q = np.linalg.svd(image)
-print(L.shape)
 # L为800的一位数组，使用diag()输出以L为对角线的矩阵
 tmp = np.diag(L)
-print(tmp.shape)
 if m < n:
     # 水平堆叠,目的就是为了将d扩充为mxn大小
     d = np.hstack((tmp, np.zeros((m, n - m))))
@@ -20,7 +19,6 @@ else:
     # 垂直堆叠
     d = np.vstack((tmp, np.zeros((m - n, n))))
 
-print(d.shape)
 k = 50
 
 image2 = P[:, :k].dot(d[:k, :k]).dot(Q[:k, :])
