8f25aae13f203ce2d441faf57feaaeae9a1c14a1
tangchunbo <532066967@qq.com>
2019-11-05 18:36:50 +0800

    add gradient

---
+++ ml/linearRegression/LinearRegression.py
@@ -0,0 +1,96 @@
+import numpy as np
+from metrics import r2_score
+
+
+class LinearRegression:
+    def __init__(self):
+        """
+        coef_ : 模型系数 （Theta1 - ThetaN）
+        intercepti_: 模型截距 Theta0
+        _theta: Theta
+        :return:
+        """
+        self.coef_ = None
+        self.intercept_ = None
+        self._theta = None
+
+    def fit_normal(self, X_train, y_train):
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        self._theta = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y_train)
+
+        self.intercept_ = self._theta[0]
+        self.coef_ = self._theta[1:]
+        return self
+
+    def fit_gd(self, X_train, y_train, eta=0.01, n_iters=4):
+        def J(theta, X_b, y):
+            try:
+                return np.sum((y - X_b.dot(theta)) ** 2) / len(y)
+            except Exception:
+                return float('inf')
+
+        def dJ(theta, X_b, y):
+            return X_b.T.dot(X_b.dot(theta) - y) * 2. / len(y)
+
+        def gradient_descent(X_b, y, init_theta, epsilon=1e-8):
+            theta = init_theta
+            i_iter = 0
+
+            while i_iter < n_iters:
+                gradient = dJ(theta, X_b, y)
+                last_theta = theta
+                theta = theta - eta * gradient
+                i_iter += 1
+                if abs(J(theta, X_b, y) - J(last_theta, X_b, y)) < epsilon:
+                    break
+            return theta
+
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        initial_theta = np.zeros(X_b.shape[1])
+        self._theta = gradient_descent(X_b, y_train, initial_theta)
+
+        self.intercept_ = self._theta[0]
+        self.coef_ = self._theta[1:]
+        return self
+
+    def fit_sgd(self, X_train, y_train, n_iters=5, t0=5, t1=50):
+        def dJ_sgd(theta, X_b_i, y_i):
+            return X_b_i * (X_b_i.dot(theta) - y_i) * 2.
+
+        def sgd(X_b, y, initial_theta, n_iters, t0=5, t1=50):
+
+            def learning_rate(t):
+                return t0 / (t + t1)
+
+            theta = initial_theta
+            m = len(X_b)
+
+            for cur_iter in range(n_iters):
+                indexes = np.random.permutation(m)
+                X_b_new = X_b[indexes]
+                y_new = y[indexes]
+                for i in range(m):
+                    gradient = dJ_sgd(theta, X_b_new[i], y_new[i])
+                    theta = theta - learning_rate(cur_iter * m + i) * gradient
+
+            return theta
+
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        initial_theta = np.random.randn(X_b.shape[1])
+        self._theta = sgd(X_b, y_train, initial_theta, n_iters, t0, t1)
+
+        self.intercept_ = self._theta[0]
+        self.coef_ = self._theta[1:]
+
+        return self
+
+    def predict(self, X_predict):
+        X_b = np.hstack([np.ones((len(X_predict), 1)), X_predict])
+        return X_b.dot(self._theta)
+
+    def score(self, X_test, y_test):
+        y_predict = self.predict(X_test)
+        return r2_score(y_test, y_predict)
+
+    def __repr__(self):
+        return "LinearRegression"
